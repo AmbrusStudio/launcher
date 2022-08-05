936e350b86d464892fb0c675a799c42178f65ef1
xiaotian <istianlei@qq.com>
2022-08-05 15:37:26 +0800

    fix: remove connect code

--- src/pages/account/nft/index.tsx
+++ src/pages/account/nft/index.tsx
@@ -1,19 +1,9 @@
 import { Stack } from '@mui/material'
-import { useEthers } from '@usedapp/core'
-import { useEffect } from 'react'
 
 import NFTItem from '../../../components/NFT/NFTItem'
 import { NFTs } from '../../../data'
 
 function AccountNFT() {
-  const { account, activateBrowserWallet } = useEthers()
-  console.log('account', account)
-
-  useEffect(() => {
-    // TODO: Porting “website repo” wallet function or redefining
-    ;(window as any).activateBrowserWallet = activateBrowserWallet
-  }, [activateBrowserWallet])
-
   return (
     <div className="my-0 mx-auto px-2.5 py-22 lg:py-32.5 max-w-[1140px] font-sans">
       <h1 className="font-bold text-9 sm:text-16 mx-0 mt-0 mb-4.5 sm:mb-9 text-white not-italic uppercase leading-12.5 sm:leading-19.5">
