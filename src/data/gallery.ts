9b42ccff27894e6f3635bb8f4209de6ea43a6ac0
xiaotian <istianlei@qq.com>
2022-08-08 20:43:50 +0800

    feat: gallery filter toggle

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,4 +1,4 @@
-import { GALLERY } from '../types/gallery'
+import { GALLERY, GALLERY_FILTER } from '../types/gallery'
 
 export const GALLERYS: GALLERY[] = [
   {
@@ -48,5 +48,86 @@ export const GALLERYS: GALLERY[] = [
   {
     id: 1,
     image: 'https://i.imgur.com/yfUga0u.png',
+  },
+]
+
+export const GALLERYS_FILTERS: GALLERY_FILTER[] = [
+  {
+    label: 'Character',
+    list: [
+      {
+        label: 'Rin',
+        count: 1111,
+      },
+      {
+        label: 'Kit',
+        count: 1111,
+      },
+      {
+        label: 'Thorn',
+        count: 1111,
+      },
+    ],
+  },
+  {
+    label: 'HAIR',
+    list: [
+      {
+        label: 'Cool Girl - Black',
+        count: 713,
+      },
+      {
+        label: 'Cool Girl - Pink',
+        count: 309,
+      },
+      {
+        label: 'Cool Girl - Gold',
+        count: 1,
+      },
+    ],
+  },
+  {
+    label: 'FACE ACCESSORIES',
+    list: [],
+  },
+  {
+    label: 'EYES',
+    list: [],
+  },
+  {
+    label: 'MAKEUP',
+    list: [],
+  },
+  {
+    label: 'EARRINGS',
+    list: [],
+  },
+  {
+    label: 'HAT',
+    list: [],
+  },
+  {
+    label: 'SKIN',
+    list: [],
+  },
+  {
+    label: 'TATTOO',
+    list: [],
+  },
+  {
+    label: 'CLOTHING',
+    list: [],
+  },
+  {
+    label: 'NECK ACCESSORIES',
+    list: [],
+  },
+  {
+    label: 'SHOULDER ACCESSORIES',
+    list: [],
+  },
+  {
+    label: 'BACK ACCESSORIES',
+    list: [],
   },
 ]
--- src/types/gallery.ts
+++ src/types/gallery.ts
@@ -2,3 +2,13 @@ export type GALLERY = {
   id: number
   image: string
 }
+
+export type GALLERY_FILTER_LIST = {
+  label: string
+  count: number
+}
+
+export type GALLERY_FILTER = {
+  label: string
+  list: GALLERY_FILTER_LIST[]
+}
