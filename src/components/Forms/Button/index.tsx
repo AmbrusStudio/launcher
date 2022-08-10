8e20bc980e07ef9a83c4388090ee52b94990be93
Garfield550 <openpgp@550.moe>
2022-08-10 17:55:32 +0800

    fix: button hover style on disabled state

--- src/components/Forms/Button/index.tsx
+++ src/components/Forms/Button/index.tsx
@@ -20,7 +20,9 @@ export function Button(props: ButtonProps) {
       className={classNames(
         'box-border uppercase w-full px-24px py-20px',
         'font-semibold text-16px leading-20px text-center',
-        'disabled:bg-grey-medium disabled:text-white disabled:hover:text-white',
+        'disabled:bg-grey-medium disabled:text-white',
+        'disabled:hover:bg-grey-medium disabled:hover:text-white',
+        'disabled:cursor-not-allowed',
         variants[variant],
         className
       )}
