b5d4c505df06941f36b2730765e2b895cdcbd96d
xiaotian <istianlei@qq.com>
2022-08-11 15:06:07 +0800

    feat: mobile

---
+++ src/components/Icon/FilterSliderLine/index.tsx
@@ -0,0 +1,14 @@
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon'
+
+const FilterSliderLine = (props: SvgIconProps) => {
+  return (
+    <SvgIcon {...props} width="52" height="52" viewBox="0 0 52 52" fill="none">
+      <path
+        d="M23.3333 34H28.6667V31.3333H23.3333V34ZM14 18V20.6667H38V18H14ZM18 27.3333H34V24.6667H18V27.3333Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  )
+}
+
+export default FilterSliderLine
--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -9,6 +9,7 @@ import logo from '../../assets/images/logo.png'
 import FilterChecked from '../../components/Icon/FilterChecked'
 import FilterClose from '../../components/Icon/FilterClose'
 import FilterOpen from '../../components/Icon/FilterOpen'
+import FilterSliderLine from '../../components/Icon/FilterSliderLine'
 import Opensea from '../../components/Icon/Opensea'
 import ModalGallery from '../../components/ModalGallery'
 import { GALLERY_INFO, GALLERYS, GALLERYS_FILTERS } from '../../data'
@@ -110,12 +111,16 @@ function Gallery() {
 
   return (
     <>
-      <div className="max-w-[1264px] m-x-auto mt-[188px]">
+      <div className="max-w-[1312px] m-x-auto mt-[68px] xl:mt-[188px] p-6">
         <div className="flex justify-between">
-          <img src={logo} className="w-[60px] h-[60px] mr-3 mt-[9px]" />
+          <img src={logo} className="hidden xl:block w-[60px] h-[60px] mr-3 mt-[9px]" />
           <div>
-            <p className="text-[64px] leading-[78px] font-bold uppercase text-white">{GALLERY_INFO.title}</p>
-            <p className="text-[64px] leading-[78px] font-bold uppercase text-rust">{GALLERY_INFO.description}</p>
+            <p className="text-[32px] xl:text-[64px] leading-[39px] xl:leading-[78px] font-bold uppercase text-white">
+              {GALLERY_INFO.title}
+            </p>
+            <p className="text-[32px] xl:text-[64px] leading-[39px] xl:leading-[78px] font-bold uppercase text-rust">
+              {GALLERY_INFO.description}
+            </p>
           </div>
           <a
             href={GALLERY_INFO.opensea_url}
@@ -125,23 +130,31 @@ function Gallery() {
           >
             <Opensea
               sx={{
-                fontSize: '52.5px',
+                fontSize: {
+                  xs: '31.5px',
+                  lg: '52.5px',
+                },
               }}
             />
           </a>
         </div>
 
-        <div className="flex justify-between my-[48px]">
-          <div className="w-[300px]">
-            <div className="p-6 m-b-4.5 bg-black/20 flex items-center">
-              <span className="text-4xl font-bold uppercase text-white leading-11">#</span>
-              <input
-                placeholder="ID"
-                className="bg-transparent text-white outline-none ml-4 leading-11 uppercase font-bold not-italic text-4xl w-[100%]
+        <div className="flex flex-col xl:flex-row justify-between my-3 xl:my-[48px]">
+          <div className="xl:w-[300px]">
+            <div>
+              <div className="w-[52px] h-[52px] rounded bg-white hidden">
+                <FilterSliderLine className="text-black" />
+              </div>
+              <div className="p-6 m-b-4.5 bg-black/20 flex items-center">
+                <span className="text-4xl font-bold uppercase text-white leading-11">#</span>
+                <input
+                  placeholder="ID"
+                  className="bg-transparent text-white outline-none ml-4 leading-11 uppercase font-bold not-italic text-4xl w-[100%]
               placeholder:uppercase placeholder:font-bold placeholder:not-italic placeholder:text-4xl placeholder:opacity-20 placeholder:text-white"
-                value={searchId}
-                onChange={(e) => setSearchId(e.target.value)}
-              />
+                  value={searchId}
+                  onChange={(e) => setSearchId(e.target.value)}
+                />
+              </div>
             </div>
             <div className="border-y-2 border-rust p-y-4 text-xl font-bold leading-6 uppercase text-white">Filters</div>
             <ul className="select-none">
@@ -208,7 +221,7 @@ function Gallery() {
           <div>
             <div className="flex items-center	justify-between">
               <div
-                className="w-[120px] h-8 rounded-2xl bg-white/10 flex items-center justify-center cursor-pointer"
+                className="p-x-4 p-y-2 rounded-2xl bg-white/10 hidden xl:flex items-center justify-center cursor-pointer"
                 onClick={() => {
                   setGalleryFilter([])
                   initFilterStatus()
@@ -222,13 +235,13 @@ function Gallery() {
               </p>
             </div>
             {
-              <div className="w-[928px] mt-6">
+              <div className="xl:w-[928px] mt-3 xl:mt-6">
                 {!!currentGallery.length && (
-                  <div className="grid grid-cols-4 gap-3">
+                  <div className="grid grid-cols-2 xl:grid-cols-4 gap-3">
                     {currentGallery.map((gallery, index) => (
                       <div
                         key={index}
-                        className="w-[223px] h-[223px] rounded overflow-hidden relative cursor-pointer"
+                        className="w-[100%] h-[100%] rounded overflow-hidden relative cursor-pointer"
                         onClick={() => {
                           setCurrentNFTInfo(currentGallery[index])
                           setVisibleNFT(true)
