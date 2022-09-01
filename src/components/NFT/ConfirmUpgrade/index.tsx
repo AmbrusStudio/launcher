a3c3192acb5f71f0f2f248b3da904bc4698d2ecb
_XiaoTian <istianlei@qq.com>
2022-09-02 02:39:17 +0800

    feat: stake unStake style, filter desc (#19)
    * feat: filter category sort desc
    
    * feat: stake unStake loading, snackbar, button style
    
    * feat: filter category sort desc
    
    * feat: stake unStake loading, snackbar, button style

--- src/components/NFT/ConfirmUpgrade/index.tsx
+++ src/components/NFT/ConfirmUpgrade/index.tsx
@@ -4,23 +4,6 @@ import { FC } from 'react'
 
 import ModalCustom from '../../ModalCustom'
 
-const InfoButton = styled.button<{ color: string }>`
-  background: ${(p) => p.color || '#ff4125'};
-  font-family: 'Montserrat', sans-serif;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 20px;
-  text-align: center;
-  text-transform: uppercase;
-  color: #ffffff;
-  border: none;
-  outline: none;
-  width: 100%;
-  min-height: 60px;
-  cursor: pointer;
-`
-
 const ConfirmationDescription = styled.p`
   font-family: 'Montserrat', sans-serif;
   font-style: normal;
@@ -35,10 +18,10 @@ const ConfirmationDescription = styled.p`
 interface ConfirmUpgradeProps {
   readonly visible: boolean
   toggle: (value: boolean) => void
-  unstake: () => void
+  confirm: () => void
 }
 
-const ConfirmUpgrade: FC<ConfirmUpgradeProps> = ({ visible, toggle, unstake }) => {
+const ConfirmUpgrade: FC<ConfirmUpgradeProps> = ({ visible, toggle, confirm }) => {
   return (
     <ModalCustom title={'Upgrade your NFT?'} visible={visible} toggle={(value) => toggle(value)}>
       <Stack
@@ -52,12 +35,12 @@ const ConfirmUpgrade: FC<ConfirmUpgradeProps> = ({ visible, toggle, unstake }) =
           You’ll earn a lot more benefits after upgrading your NFT. You can only upgrade this NFT whilel it’s still
           staked.
         </ConfirmationDescription>
-        <InfoButton color="#FF4125" onClick={() => unstake()}>
+        <button className="u-btn u-btn-primary" onClick={() => confirm()}>
           Upgrade and unstake
-        </InfoButton>
-        <InfoButton color="#2A2A2A" onClick={() => toggle(false)}>
+        </button>
+        <button className="u-btn" onClick={() => toggle(false)}>
           Cancel
-        </InfoButton>
+        </button>
       </Stack>
     </ModalCustom>
   )
