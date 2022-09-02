3318effcf3cdfe3f6d1d92845afa1871edce0bb3
xiaotian <istianlei@qq.com>
2022-09-02 18:37:41 +0800

    feat: format seconds

---
+++ src/utils/time.ts
@@ -0,0 +1,38 @@
+/**
+ * formatted seconds
+ * @param value
+ * @returns formatted string
+ */
+export function formatSeconds(value: string) {
+  let theTime = parseInt(value) // Time seconds to convert
+  let theTimeMinute = 0 // minute
+  let theTimeHour = 0 // hour
+  let theTimeDay = 0 // day
+  if (theTime > 60) {
+    theTimeMinute = parseInt(String(theTime / 60))
+    theTime = parseInt(String(theTime % 60))
+    if (theTimeMinute > 60) {
+      theTimeHour = parseInt(String(theTimeMinute / 60))
+      theTimeMinute = parseInt(String(theTimeMinute % 60))
+      if (theTimeHour > 24) {
+        // more than 24 hours
+        theTimeDay = parseInt(String(theTimeHour / 24))
+        theTimeHour = parseInt(String(theTimeHour % 24))
+      }
+    }
+  }
+  let result = ''
+  if (theTime > 0) {
+    result = '' + parseInt(String(theTime)) + ' second'
+  }
+  if (theTimeMinute > 0) {
+    result = '' + parseInt(String(theTimeMinute)) + ' minute ' + result
+  }
+  if (theTimeHour > 0) {
+    result = '' + parseInt(String(theTimeHour)) + ' hour ' + result
+  }
+  if (theTimeDay > 0) {
+    result = '' + parseInt(String(theTimeDay)) + ' day ' + result
+  }
+  return result
+}
