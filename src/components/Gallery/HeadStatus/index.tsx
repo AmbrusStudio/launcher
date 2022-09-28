88063b867e018cef5584a79d1fc3ff0c9007aba3
xiaotian <istianlei@qq.com>
2022-09-28 21:50:51 +0800

    feat: head, search, count sticky top

--- src/components/Gallery/HeadStatus/index.tsx
+++ src/components/Gallery/HeadStatus/index.tsx
@@ -1,19 +1,35 @@
 import classNames from 'classnames'
 import numbro from 'numbro'
-import { FC } from 'react'
+import { FC, RefObject, useMemo } from 'react'
+
+import { Position } from '../../../utils/scroll'
 
 interface Props {
-  readonly isFixed: boolean
   readonly count: number
+  readonly scroll: Position | undefined
+  readonly wrapperRef: RefObject<HTMLDivElement>
   clearFilter: () => void
 }
 
-const HeadStatus: FC<Props> = ({ isFixed, count, clearFilter }) => {
+const HeadStatus: FC<Props> = ({ scroll, wrapperRef, count, clearFilter }) => {
+  const isFixed = useMemo(() => {
+    const headerDom = document.querySelector<HTMLDivElement>('#header')
+    const headerHeight = headerDom?.offsetHeight || 100
+    const mql = window.matchMedia('(min-width: 1024px)')
+    // 12 mobile
+    // 24 pc
+    const padding = mql.matches ? 24 : 12
+    return scroll && wrapperRef?.current ? scroll.top >= wrapperRef.current?.offsetTop - headerHeight + padding : false
+  }, [scroll, wrapperRef])
+
   return (
     <div
-      className={classNames('flex items-center justify-between sticky top-[120px] lg:top-[68px] xl:top-[100px] z-1', {
-        'bg-[#252525] py-3': isFixed,
-      })}
+      className={classNames(
+        'flex items-center justify-between sticky top-[120px] lg:top-[68px] xl:top-[100px] py-3 z-1',
+        {
+          'bg-[#252525]': isFixed,
+        }
+      )}
     >
       <div
         className="p-x-4 p-y-2 rounded-2xl bg-white/10 hidden lg:flex items-center justify-center cursor-pointer text-sm font-medium text-center leading-4.25 text-white"
