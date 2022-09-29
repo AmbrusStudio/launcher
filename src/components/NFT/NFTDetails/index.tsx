c57b794e821ca0beccd8b905dd8ffa73833d902f
_XiaoTian <istianlei@qq.com>
2022-09-29 23:24:12 +0800

    feat: set blind box (#42)

--- src/components/NFT/NFTDetails/index.tsx
+++ src/components/NFT/NFTDetails/index.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 import { Stack } from '@mui/material'
 import Skeleton from '@mui/material/Skeleton'
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 
-import { NFTE4CRanger } from '../../../types'
+import { NFTE4CRanger, Trait, TraitItem } from '../../../types'
 import { getEdition } from '../../../utils'
 import NFTTag from '../NFTTag'
 
@@ -43,16 +43,25 @@ interface NFTDetailsProps {
 }
 
 const NFTDetails: FC<NFTDetailsProps> = ({ nft, tokenId }) => {
+  // Name moves to the top
+  const traitSort = useMemo<TraitItem[]>(() => {
+    const index = nft.attributes.findIndex((i) => i.trait_type === Trait.Name)
+    if (~index) {
+      nft.attributes.unshift(nft.attributes.splice(index, 1)[0])
+    }
+    return nft.attributes
+  }, [nft.attributes])
+
   return (
     <>
       <section className="flex items-start justify-between">
         <section className="mr-2">
           <span className="font-bold text-base lg:text-9 not-italic uppercase leading-5 lg:leading-11 font-sans text-rust break-all">
-            {nft.name}
+            E4C Rangers
           </span>
           <Stack spacing={2} direction="row">
             <span className="font-bold text-base lg:text-9 not-italic uppercase leading-5 lg:leading-11 font-sans text-white break-all">
-              {'-'}
+              Series 1
             </span>
             <span className="font-bold text-base lg:text-9 not-italic uppercase leading-5 lg:leading-11 font-sans text-white">
               #{tokenId}
@@ -66,15 +75,17 @@ const NFTDetails: FC<NFTDetailsProps> = ({ nft, tokenId }) => {
             className="w-[100px] lg:w[140px] !h-[40px] !lg:h-[58px]"
           />
         ) : (
-          <NFTTag content={getEdition(Number(tokenId), nft.upgraded)} />
+          <NFTTag content={getEdition(nft.upgraded)} />
         )}
       </section>
 
       <NFTInfoContent>
-        {nft.attributes.map((trait, index) => (
+        {traitSort.map((trait, index) => (
           <section key={index}>
             <NFTInfoIntroductionTitle>{trait.trait_type}</NFTInfoIntroductionTitle>
-            <NFTInfoIntroductionContent>{trait.value}</NFTInfoIntroductionContent>
+            <NFTInfoIntroductionContent>
+              {trait.trait_type === Trait.Name ? trait.value : 'unknown'}
+            </NFTInfoIntroductionContent>
           </section>
         ))}
       </NFTInfoContent>
