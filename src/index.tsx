ba1e2b5a020653c67fee1075778aa40ace7ceaa0
xiaotian <istianlei@qq.com>
2022-10-09 19:55:58 +0800

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 import CssBaseline from '@mui/material/CssBaseline'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -13,6 +15,17 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { Provider } from './components/Provider'
 import reportWebVitals from './reportWebVitals'
+
+Sentry.init({
+  enabled: import.meta.env.PROD,
+  dsn: 'https://1c5c34432dc74095b57a61836bde45be@o327345.ingest.sentry.io/4503953256742912',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
