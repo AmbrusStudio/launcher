07fdb34f0ea1e0d93dd1885812861464efc8d020
xiaotian <istianlei@qq.com>
2022-10-11 12:14:49 +0800

    fix: get all nfts

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,3 +1,5 @@
 export type RemoveIndex<Q> = {
   [key in keyof Q as string extends key ? never : key extends string ? key : never]: Q[key]
 }
+
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] }
