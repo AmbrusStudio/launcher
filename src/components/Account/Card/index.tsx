e6cd595305683e2aa36e8faebd919a035a49d16e
Garfield550 <openpgp@550.moe>
2022-10-27 09:42:30 +0000

    style: account center mobile style

--- src/components/Account/Card/index.tsx
+++ src/components/Account/Card/index.tsx
@@ -8,7 +8,7 @@ type AccountCardProps = {
 export function AccountCard(props: React.PropsWithChildren<AccountCardProps>) {
   const { className, children, title } = props
   return (
-    <section className={classNames('flex flex-col p-36px bg-card', className)}>
+    <section className={classNames('flex flex-col p-24px lg:p-36px bg-card', className)}>
       {title && <h3 className="font-bold text-24px leading-30px uppercase text-white">{title}</h3>}
       {children}
     </section>
