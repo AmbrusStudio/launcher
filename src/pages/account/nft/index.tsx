44294b81f7ffab764caf06d9d66123d7729abf79
xiaotian <istianlei@qq.com>
2022-11-09 19:39:40 +0800

    fix: test nft show, fix get metadata

--- src/pages/account/nft/index.tsx
+++ src/pages/account/nft/index.tsx
@@ -2,7 +2,7 @@ import { Stack } from '@mui/material'
 import CircularProgress from '@mui/material/CircularProgress'
 import { shortenIfAddress, useEthers } from '@usedapp/core'
 import { useUpdate } from 'ahooks'
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
 import { AccountCenterPageLayout } from '../../../components/Layout'
@@ -11,17 +11,8 @@ import MobileWrap from '../../../components/NFT/MobileWrap'
 import NFTItem from '../../../components/NFT/NFTItem'
 import Perk from '../../../components/NFT/Perk'
 import PerkModal from '../../../components/NFT/PerkModal'
-import {
-  ADDRESS_E4C_Ranger_Gold_Edition,
-  ADDRESS_E4C_Ranger_Rangers_Edition,
-  ADDRESS_E4CRanger_Gold_Holder,
-  ADDRESS_E4CRanger_Rangers_Holder,
-  ADDRESS_ImmutableX_E4C_Ranger_Gold_Edition,
-  ADDRESS_ImmutableX_E4C_Ranger_Rangers_Edition,
-} from '../../../contracts'
 import { useWeb3Modal } from '../../../hooks'
-import { useERC721ImmutableXListState, useERC721ListState } from '../../../hooks/useERC721List'
-import { useMetadataBaseURL } from '../../../hooks/useMetadataBaseURL'
+import { useUserStakeCollections } from '../../../hooks/useUserStakeCollections'
 
 function AccountNFT() {
   const { account, active } = useEthers()
@@ -30,36 +21,8 @@ function AccountNFT() {
   const isMd = useMediaQuery({ query: '(min-width: 768px)' })
   const update = useUpdate()
 
-  const { metadadaGoldBaseURI, metadadaRangersBaseURI } = useMetadataBaseURL()
-
-  const { nfts: nftsGold, loading: loadingGold } = useERC721ListState({
-    holderAddress: ADDRESS_E4CRanger_Gold_Holder,
-    tokenAddress: ADDRESS_E4C_Ranger_Gold_Edition,
-    baseURL: metadadaGoldBaseURI,
-  })
-  const { nfts: nftsRangers, loading: loadingRangers } = useERC721ListState({
-    holderAddress: ADDRESS_E4CRanger_Rangers_Holder,
-    tokenAddress: ADDRESS_E4C_Ranger_Rangers_Edition,
-    baseURL: metadadaRangersBaseURI,
-  })
-  const { nfts: nftsImmutableXGold, loading: loadingImmutableXGold } = useERC721ImmutableXListState({
-    collection: ADDRESS_ImmutableX_E4C_Ranger_Gold_Edition,
-    baseURL: metadadaGoldBaseURI,
-  })
-  const { nfts: nftsImmutableXRangers, loading: loadingImmutableXRangers } = useERC721ImmutableXListState({
-    collection: ADDRESS_ImmutableX_E4C_Ranger_Rangers_Edition,
-    baseURL: metadadaRangersBaseURI,
-  })
-
-  const nfts = useMemo(
-    () => [...nftsGold, ...nftsRangers, ...nftsImmutableXGold, ...nftsImmutableXRangers],
-    [nftsGold, nftsRangers, nftsImmutableXGold, nftsImmutableXRangers]
-  )
-  const loading = useMemo(() => {
-    return loadingGold && loadingRangers && loadingImmutableXGold && loadingImmutableXRangers
-  }, [loadingGold, loadingRangers, loadingImmutableXGold, loadingImmutableXRangers])
-
-  nfts.length && console.log('nfts', nfts)
+  const { collections, loading } = useUserStakeCollections()
+  collections.length && console.log('collections', collections)
 
   const [visiblePerk, setVisiblePerk] = useState<boolean>(false)
   const [visiblePerkModal, setVisiblePerkModal] = useState<boolean>(false)
@@ -83,18 +46,18 @@ function AccountNFT() {
             }}
           />
         </div>
-      ) : account && active && !loading && !nfts.length ? (
+      ) : account && active && !loading && !collections.length ? (
         <div className="text-white">No Data...</div>
       ) : (
         <>
           {isMd ? (
             <Stack spacing={3}>
-              {nfts.map((nft) => (
+              {collections.map((nft) => (
                 <NFTItem nft={nft} key={`${nft.address}_${nft.tokenId}`} tokenId={nft.tokenId} update={update} />
               ))}
             </Stack>
           ) : (
-            <MobileWrap nfts={nfts} update={update} />
+            <MobileWrap nfts={collections} update={update} />
           )}
         </>
       )}
