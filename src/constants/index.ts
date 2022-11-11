49375fca1fcca69b30dc09a50dc5c703c41c4f25
xiaotian <istianlei@qq.com>
2022-11-11 12:41:34 +0800

    feat: immutableXWithdraw

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -20,6 +20,3 @@ export const Blindbox = {
     BlindBoxPictures: 'https://ambrus.s3.amazonaws.com/1667538431877_0.51_blindbox.gif',
   },
 }
-
-// ImmutableX Withdraw
-export const ImmutableXWithdraw = 'https://market.immutable.com/'
