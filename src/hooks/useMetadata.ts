db44b0716c9c8cacbfda0f288b660d3076609391
xiaotian <istianlei@qq.com>
2022-11-23 00:37:06 +0800

    style: rm getMetadataByAddress

--- src/hooks/useMetadata.ts
+++ src/hooks/useMetadata.ts
@@ -1,20 +1,12 @@
 import { Falsy } from '@usedapp/core'
 import { useDeepCompareEffect } from 'ahooks'
-import { getAddress } from 'ethers/lib/utils'
 import { useCallback, useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { metadataApi } from '../api/metadata'
-import {
-  defaultChainId,
-  E4CRanger_GoldEdition,
-  E4CRanger_ImmutableX_GoldEdition,
-  E4CRanger_ImmutableX_RangersEdition,
-  E4CRanger_RangersEdition,
-  E4CRanger_UltimateEdition,
-} from '../contracts'
+import { defaultChainId } from '../contracts'
 import { RootState } from '../store'
-import { MetadataResponse, MetadataStatus, TokenMetadata } from '../types'
+import { MetadataResponse, TokenMetadata } from '../types'
 import { buildMetadataInformation } from '../utils'
 
 /**
@@ -46,14 +38,6 @@ export function useMetadata() {
     (state: RootState) => state.metadataImmutableX.RangersEditionLoading
   )
 
-  const metadataGold = useMemo<TokenMetadata[]>(
-    () => [...metadadaGoldEdition.metadata, ...metadataImmutableXGoldEdition.metadata],
-    [metadadaGoldEdition.metadata, metadataImmutableXGoldEdition.metadata]
-  )
-  const metadadaRangers = useMemo<TokenMetadata[]>(
-    () => [...metadadaRangersEdition.metadata, ...metadataImmutableXRangersEdition.metadata],
-    [metadadaRangersEdition.metadata, metadataImmutableXRangersEdition.metadata]
-  )
   const metadataAllEdition = useMemo<TokenMetadata[]>(
     () => [
       ...metadadaUltimateEdition.metadata,
@@ -87,58 +71,11 @@ export function useMetadata() {
     ]
   )
 
-  /**
-   * Get Metadata By Address
-   * TODO Obtain on demand through the API interface
-   * @param address
-   * @returns
-   */
-  const getMetadataByAddress = useCallback(
-    (address: string | Falsy, status: MetadataStatus): TokenMetadata[] => {
-      if (!address) {
-        return metadataAllEdition
-      }
-
-      if (status === MetadataStatus.Ethereum) {
-        if (
-          getAddress(address) === getAddress(E4CRanger_GoldEdition) ||
-          getAddress(address) === getAddress(E4CRanger_ImmutableX_GoldEdition)
-        ) {
-          return metadataGold
-        } else if (
-          getAddress(address) === getAddress(E4CRanger_RangersEdition) ||
-          getAddress(address) === getAddress(E4CRanger_ImmutableX_RangersEdition)
-        ) {
-          return metadadaRangers
-        } else if (getAddress(address) === getAddress(E4CRanger_UltimateEdition)) {
-          return metadadaUltimateEdition.metadata
-        } else {
-          console.error(`metadata not found, Ethereum status: ${status}`)
-          return metadataAllEdition
-        }
-      } else if (status === MetadataStatus.ImmutableX) {
-        if (getAddress(address) === getAddress(E4CRanger_ImmutableX_GoldEdition)) {
-          return metadataGold
-        } else if (getAddress(address) === getAddress(E4CRanger_ImmutableX_RangersEdition)) {
-          return metadadaRangers
-        } else {
-          console.error(`metadata not found, ImmutableX status: ${status}`)
-          return metadataAllEdition
-        }
-      } else {
-        console.error(`metadata status not found, status: ${status}`)
-        return metadataAllEdition
-      }
-    },
-    [metadadaRangers, metadadaUltimateEdition.metadata, metadataAllEdition, metadataGold]
-  )
-
   return {
     metadadaGoldEdition,
     metadadaRangersEdition,
     metadataAllEdition,
     loading,
-    getMetadataByAddress,
   }
 }
 
