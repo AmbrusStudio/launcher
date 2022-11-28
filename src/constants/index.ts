74abf7c3f0893a907eaa4497195f58a8a20030b1
_XiaoTian <istianlei@qq.com>
2022-11-28 18:33:05 +0800

    feat: add blindbox countdown (#123)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,25 +1,11 @@
-import { Trait, TraitEdition, TraitName } from '../types'
+import { TraitEdition, TraitName } from '../types'
 
 /** Local Storage Key Access Token */
 export const LSK_ACCESS_TOKEN = 'access_token'
 /** Local Storage Key Immutable X Wallet Info */
 export const LSK_IMX_WALLET_INFO = 'imx_wallet_info'
 
-// Blind box
-export const BlindBoxTrait = [Trait.Name]
-
-export const Blindbox = {
-  [TraitName.Rin]: {
-    BlindBoxMode: false,
-    BlindBoxVideo: 'https://cdn.ambrus.studio/NFTs/Blindbox.mp4',
-    BlindBoxPictures: 'https://cdn.ambrus.studio/NFTs/blind_box.jpg',
-  },
-  [TraitName.Kit]: {
-    BlindBoxMode: false,
-    BlindBoxVideo: 'https://ambrus.s3.amazonaws.com/1667537607898_0.88_Blindbox.mp4',
-    BlindBoxPictures: 'https://ambrus.s3.amazonaws.com/1667538431877_0.51_blindbox.gif',
-  },
-}
+export * from './blindbox'
 
 /**
  * Role Number <Rin | Kit>
