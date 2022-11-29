240cda47acd540d556064eb6af014750085f49ea
xiaotian <istianlei@qq.com>
2022-11-29 18:52:59 +0800

    feat: web3modal cachedProvider

--- src/hooks/useWeb3Modal.ts
+++ src/hooks/useWeb3Modal.ts
@@ -1,5 +1,7 @@
 import { useEthers } from '@usedapp/core'
+import { useMount } from 'ahooks'
 import React from 'react'
+import store from 'store'
 import Web3Modal from 'web3modal'
 
 import { web3ModalProviderOptions } from '../contracts'
@@ -10,27 +12,34 @@ const defaultChainId = getDefaultChainId()
 type UseWeb3Modal = {
   readonly chainIdMismatch: boolean
   connect: () => Promise<void>
+  disconnect: () => Promise<void>
   switchNetwork: () => Promise<void>
 }
 
+const web3Modal = new Web3Modal({
+  cacheProvider: true,
+  providerOptions: web3ModalProviderOptions,
+})
+
 export function useWeb3Modal(): UseWeb3Modal {
-  const { chainId, activate, switchNetwork: dappSwitchNetwork } = useEthers()
+  const { chainId, activate, switchNetwork: dappSwitchNetwork, deactivate } = useEthers()
 
   const chainIdMismatch = chainId !== defaultChainId
 
-  const connect = React.useCallback(async () => {
-    const web3Modal = new Web3Modal({
-      providerOptions: web3ModalProviderOptions,
-    })
-
-    // remove historical influence
-    if (web3Modal.cachedProvider) {
-      web3Modal.clearCachedProvider()
-    }
-
+  const connect = async () => {
     const provider = await web3Modal.connect()
     await activate(provider)
-  }, [activate])
+  }
+
+  const disconnect = async () => {
+    if (web3Modal.cachedProvider) {
+      if (web3Modal.cachedProvider === 'walletconnect') {
+        store.remove('walletconnect')
+      }
+      await web3Modal.clearCachedProvider()
+    }
+    deactivate()
+  }
 
   const switchNetwork = React.useCallback(async () => {
     if (chainIdMismatch) await dappSwitchNetwork(defaultChainId)
@@ -38,11 +47,18 @@ export function useWeb3Modal(): UseWeb3Modal {
     window.location.reload()
   }, [chainIdMismatch, dappSwitchNetwork])
 
+  useMount(() => {
+    if (web3Modal.cachedProvider) {
+      connect()
+    }
+  })
+
   return {
     get chainIdMismatch() {
       return chainIdMismatch
     },
     connect,
+    disconnect,
     switchNetwork,
   }
 }
