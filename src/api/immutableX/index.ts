fbacbfb06e9fcba2d2124f800071edf08c25bdcc
Garfield550 <openpgp@550.moe>
2022-12-20 06:37:05 +0000

    chore: update comment

--- src/api/immutableX/index.ts
+++ src/api/immutableX/index.ts
@@ -62,7 +62,7 @@ export async function immutableXTransferConfirmHiveApi<T = unknown>({ id }: { id
 
 /**
  * Get ImmutableX StakingStatus Api
- * discard do not use
+ * @deprecated do not use
  * @param tokenAddress
  * @param tokenId
  * @returns
